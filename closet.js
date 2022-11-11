715574bdd2f9bba70587f860f569e81df6cdb4f9
sagar jadhao <jadhaosagar9798@gmail.com>
2022-11-11 22:59:54 +0530

    Day-4 done

---
+++ closet.js
@@ -0,0 +1,64 @@
+
+// registration page js
+
+
+
+
+function addData(){
+
+    var Name= document.getElementById("name").value
+    var email= document.getElementById("email").value;
+    var pass = document.getElementById("pwd").value;
+    var number=document.getElementById("num").value;
+    
+    
+    localStorage.setItem("username",Name);
+    localStorage.setItem("usermail",email);
+    localStorage.setItem("userpwd",pass);
+    localStorage.setItem("usernumber",number);
+    
+    alert("REGISTRATION SUCCESSFULL")
+    
+    }
+
+
+
+
+ // log in page js 
+
+
+ 
+function checkData(){
+
+
+
+    var enteremail=document.getElementById("email").value;
+    var enterpwd=document.getElementById("pwd").value;
+
+    //get data from localstorage that we set through ragestragin page
+
+    var getEmail=localStorage.getItem("usermail");
+    var getpwd=localStorage.getItem("userpwd");
+
+
+    if(enteremail===getEmail){
+       
+        if(enterpwd===getpwd){
+            alert("LOGIN SUCCESSFUL")
+        }
+        else{
+            alert(" WRONG PASSWORD")
+        }
+   
+    }
+     else{
+        alert("INVALID DETAILS")
+     }
+
+
+
+    }
+
+
+
+
